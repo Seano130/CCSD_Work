d4899e5a2476de6f252d962b69ea774b8e847ec1
Sean O'Dell <seano544@gmail.com>
2017-09-13 14:01:01 -0700

    Test upload

---
+++ TestTimer/frmMain.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TestTimer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(34, 132);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(155, 131);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(114, 54);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(40, 45);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "0";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 295);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ TestTimer/frmMain.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestTimer
+{
+    public partial class frmMain : Form
+    {
+        protected Timer tmr;
+        protected double totalTime;
+        protected bool goingForward;
+        public frmMain()
+        {
+            InitializeComponent();
+            tmr = new Timer();
+            tmr.Interval = 100; // in milliseconds
+            tmr.Tick += tmr_Tick; // subscribe to the timers tick event
+            totalTime = 0; // start time at 0
+            lblTime.Text = totalTime.ToString(); // initialize label to time zero
+            goingForward = true;
+        }
+
+        void tmr_Tick(object sender, EventArgs e)
+        {
+            if (goingForward == true)
+            {
+                totalTime = totalTime + 0.1;
+            }
+            else
+            {
+                totalTime = totalTime - 0.1; //increment time
+            }
+            lblTime.Text = totalTime.ToString("N1"); // dump time to screen
+        }
+
+       protected void btnStart_Click(object sender, EventArgs e)
+        {
+            tmr.Start();
+
+        }
+
+       protected void btnStop_Click(object sender, EventArgs e)
+        {
+            tmr.Stop();
+            goingForward = !goingForward;
+
+          
+        }
+
+       
+       
+
+
+      
+
+    }
+}
