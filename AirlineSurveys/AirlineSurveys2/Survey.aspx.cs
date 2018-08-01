a084d12617823e731684e2560f41fd34bc9f3514
Sean O'Dell <seano544@gmail.com>
2018-08-01 14:16:54 -0700

    AirlineSurveysProject

---
+++ AirlineSurveys/AirlineSurveys2/Survey.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace AirlineSurveys2
+{
+    public partial class Survey : System.Web.UI.Page
+    {
+        MySqlConnection cnx;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            String s = @"Data Source=localhost; Database=flights; Id=rootl; Password="";SSL Mode=None";
+            cnx = new MySqlConnection(s);
+            try
+            {
+                cnx.Open();
+            }
+            catch (Exception ex)
+            {
+
+                Response.Write("Error while opening the connection - Reason = ", ex.Message);
+                return;
+
+            }
+
+
+        }
+    }
+}
