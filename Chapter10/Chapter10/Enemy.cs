3621b956fccb4728f6f933ea3f6632b333f44876
Sean O'Dell <seano544@gmail.com>
2018-08-15 13:07:55 -0700

    Example of virtual class

---
+++ Chapter10/Chapter10/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter10
+{
+    public class Enemy
+    {
+        public virtual void Talk()
+        {
+            Console.WriteLine("Grunt");
+        }
+    }
+
+    public class BigBoss : Enemy
+    {
+        //<summary>
+        // This is redefining the method talk we had in the class,
+        // such that the child class can behave differently (So BigBoss can emit different grunt
+        // instead of regular grunt). The requirements for that to take place are:
+        // - Talk needs to be virtual in the parent class\
+        // - Talk needs to be overriden in the "Derived" (child) class.
+        //</summary>
+
+        public override void Talk()
+        {
+            Console.WriteLine("Boo");
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+}
