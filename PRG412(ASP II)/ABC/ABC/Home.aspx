7919fb11a93da1219b89974b8d6906801cbd6815
Sean O'Dell <seano544@gmail.com>
2018-08-21 14:11:18 -0700

    Simple Login (ABC class project)

---
+++ PRG412(ASP II)/ABC/ABC/Home.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Home.aspx.cs" Inherits="Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="lblTitle" runat="server" Text="User"></asp:Label>
+        <br />
+        <br />
+        <asp:TextBox ID="tbUser" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btnLogin" runat="server" Height="51px" OnClick="btnLogin_Click" Text="Login" Width="105px" />
+        <br />
+        <br />
+        <asp:Button ID="btnReload" runat="server" OnClick="btnReload_Click" Text="Reload" />
+        <br />
+    </form>
+</body>
+</html>
---
+++ PRG412(ASP II)/ABC/ABC/Home.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Home : System.Web.UI.Page
+{
+   
+    protected void Page_Load(object sender, EventArgs e)
+    { // Phase A
+        if(!IsPostBack)
+        {
+            btnLogin.Text = "Login";
+        }      
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    { // Phase B
+        // Handle all future button clicks
+
+        if(Session["user"] == null) // nobody currently logged in
+        {
+            Session["user"] = tbUser.Text; // log them in
+        }
+        else
+        { // someone already logged in, so log them out
+            Session["user"] = null; // log them out
+
+
+        }
+
+
+
+
+       //if(btnLogin.Text == "Login")
+       // {
+       //     if(tbUser.Text != "")
+       //     {
+       //         lblTitle.Text = "Welcome, " + tbUser.Text + "!";
+       //         btnLogin.Text = "Logout";
+       //     }                   
+       // }
+       //else
+       // {
+       //     lblTitle.Text = "Enter Username";
+       //     tbUser.Visible = true;
+       //     lblTitle.Text = "";
+       //     btnLogin.Text = "Login";
+       // }
+    }
+
+    protected void btnReload_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Page_PreRender(object sender, EventArgs e)
+    { // Phase C
+        if(Session["user"] == null)
+        { // nobody logged in 
+            lblTitle.Text = "Enter Username: ";
+            tbUser.Visible = true;
+            btnLogin.Text = "Login";
+        }
+        else
+        { // someone already logged in
+            lblTitle.Text = "Hello " + (string)Session["user"];
+            tbUser.Visible = false;
+            btnLogin.Text = "Logout";
+        }
+
+
+
+        //if(btnLogin.Text == "Logout")
+        //{
+        //    tbUser.Visible = false;
+        //}
+    }
+
+
+ 
+}
