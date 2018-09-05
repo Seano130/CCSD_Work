8bc04f871145f83e6afd64b5ef6e6ef101c7d00b
Sean O'Dell <seano544@gmail.com>
2018-09-05 13:55:28 -0700

    Update to ASP.NET Store2018

---
+++ PRG412(ASP II)/Store2018v2/Store2018v2/App_Code/Keymaker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class Keymaker
+{
+    public Keymaker()
+    {
+        public bool CheckUserExists(string user)
+        {
+
+        }
+        
+        publc bool Check
+        
+    }
+}
